11cabeb1d7a8b7dfaf45f53d9474e1b01df7e25b
Shawn Baugh II <shawnbaughcreative@gmail.com>
2017-10-22 12:24:14 -0500

    Completed countdown project.

---
+++ countdown_prj/src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Counter from "./Components/Counter/Counter.js";
+
+class App extends React.Component{
+    render(){
+        return(
+            <Counter first_name="Bobby" />
+        )
+    }
+}
+export default App;
